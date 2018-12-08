08eba7bdc55baef917d35a91a92854f206545203
Yuta Matsumura <y-matsumura@alterbooth.com>
2018-12-08 16:39:56 +0900

    MiddlewareでDIを扱う

--- src/2.2/MiddlewareSample/Middlewares/MyMiddleware.cs
+++ src/2.2/MiddlewareSample/Middlewares/MyMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace MiddlewareSample.Middlewares
@@ -6,14 +7,17 @@ namespace MiddlewareSample.Middlewares
     public class MyMiddleware
     {
         private readonly RequestDelegate next;
+        private readonly ILogger<MyMiddleware> logger;
 
-        public MyMiddleware(RequestDelegate next)
+        public MyMiddleware(RequestDelegate next, ILogger<MyMiddleware> logger)
         {
             this.next = next;
+            this.logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
+            logger.LogInformation($"QueryString: {context.Request.QueryString}");
             await next(context);
         }
     }
