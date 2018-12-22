1f611f501ffde851cb434243442879d5deed7596
Yuta Matsumura <y-matsumura@alterbooth.com>
2018-12-22 22:30:54 +0900

    Scaffolding

---
+++ src/2.2/HealthCheckSample/Data/HealthCheckSampleContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthCheckSample.Models
+{
+    public class HealthCheckSampleContext : DbContext
+    {
+        public HealthCheckSampleContext (DbContextOptions<HealthCheckSampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HealthCheckSample.Models.Movie> Movie { get; set; }
+    }
+}
