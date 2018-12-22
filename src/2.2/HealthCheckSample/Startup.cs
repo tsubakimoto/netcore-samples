159088ab8be69bdbf7ae12ca48f674203ed214fe
Yuta Matsumura <y-matsumura@alterbooth.com>
2018-12-22 23:40:37 +0900

    Health Check (Build-in and Custom UI)

--- src/2.2/HealthCheckSample/Startup.cs
+++ src/2.2/HealthCheckSample/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HealthCheckSample.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using HealthCheckSample.Models;
 
 namespace HealthCheckSample
 {
@@ -38,6 +34,22 @@ namespace HealthCheckSample
 
             services.AddDbContext<HealthCheckSampleContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("HealthCheckSampleContext")));
+
+            // https://blogs.msdn.microsoft.com/webdev/2018/08/22/asp-net-core-2-2-0-preview1-healthcheck/
+            //services.AddHealthChecks(); //default
+
+            // BeatPlus-> https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks
+            services.AddHealthChecks()
+                .AddSqlServer(Configuration.GetConnectionString("HealthCheckSampleContext")) //AspNetCore.HealthChecks.SqlServer (default)
+                //.AddSqlServer( //AspNetCore.HealthChecks.SqlServer (manual)
+                //    connectionString: Configuration.GetConnectionString("HealthCheckSampleContext"),
+                //    healthQuery: "SELECT 1;",
+                //    name: "sql",
+                //    failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Degraded,
+                //    tags: new string[] { "db", "sql", "sqlserver" })
+                .AddAzureBlobStorage(Configuration.GetConnectionString("AzureStorage")) //AspNetCore.HealthChecks.AzureStorage
+                ;
+            services.AddHealthChecksUI(); //AspNetCore.HealthChecks.UI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +69,28 @@ namespace HealthCheckSample
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            // https://blogs.msdn.microsoft.com/webdev/2018/08/22/asp-net-core-2-2-0-preview1-healthcheck/
+            //app.UseHealthChecks("/healthz"); //default
+            //app.UseHealthChecks("/healthz", new HealthCheckOptions //customized message
+            //{
+            //    ResponseWriter = async (context, health) =>
+            //    {
+            //        await context.Response.WriteAsync("who are you?");
+            //    }
+            //});
+
+            // UI-> https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                Predicate = _ => true
+            });
+            app.UseHealthChecks("/healthz", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = HealthChecks.UI.Client.UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            app.UseHealthChecksUI(); //AspNetCore.HealthChecks.UI
 
             app.UseMvc(routes =>
             {
