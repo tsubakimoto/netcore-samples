d3e27e74a23d255f59ba5906b7e08529d9b60921
Yuta Matsumura <y-matsumura@alterbooth.com>
2019-01-17 17:44:41 +0900

    IdentityServer4を使ったサンプル

---
+++ src/2.2/Oidc/AuthorizationServer/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AuthorizationServer
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// https://christianlydemann.com/creating-identity-server-setup-with-client-credential-authentication-oidc-part-2/
+    /// </remarks>
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("resourceApi", "API Application")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "clientApp",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = { "resourceApi" }
+                }
+            };
+        }
+    }
+}
---
+++ src/2.2/Oidc/ClientApp/Controllers/IdentityController.cs
@@ -0,0 +1,48 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientApp.Controllers
+{
+    public class IdentityController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            // discover endpoints from metadata
+            var oidcDiscoveryResult = await DiscoveryClient.GetAsync("http://localhost:53780");
+            if (oidcDiscoveryResult.IsError)
+            {
+                Console.WriteLine(oidcDiscoveryResult.Error);
+                return Json(oidcDiscoveryResult.Error);
+            }
+            // request token
+            var tokenClient = new TokenClient(oidcDiscoveryResult.TokenEndpoint, "clientApp", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("resourceApi");
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                throw new HttpRequestException(tokenResponse.Error);
+            }
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = await client.GetAsync("http://localhost:53903/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+                throw new HttpRequestException(response.StatusCode.ToString());
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+                return Json(content);
+            }
+        }
+    }
+}
