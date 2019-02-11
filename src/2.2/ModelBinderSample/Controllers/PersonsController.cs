0ee48bcc382537059dd3233fe2cba10c3d787350
Yuta Matsumura <tkmbmbr@gmail.com>
2019-02-11 11:40:24 +0900

    モデルバインドのサンプルを追加した

---
+++ src/2.2/ModelBinderSample/Controllers/PersonsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBinderSample.Models;
+using ModelBinderSample.Models.Binders;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModelBinderSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<object> Post(
+            [ModelBinder(binderType: typeof(CsvModelBinder))] IEnumerable<Person> persons)
+        {
+            return new
+            {
+                ItemsRead = persons.Count(),
+                Persons = persons
+            };
+        }
+    }
+}
---
+++ src/2.2/ModelBinderSample/Models/Binders/CsvModelBinder.cs
@@ -0,0 +1,53 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinderSample.Models.Binders
+{
+    public class CsvModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            // Specify a default argument name if none is set by ModelBinderAttribute
+            var modelName = bindingContext.ModelName;
+            if (string.IsNullOrEmpty(modelName))
+            {
+                modelName = "model";
+            }
+
+            // Try to fetch the value of the argument by name
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+            var value = valueProviderResult.FirstValue;
+            // Check if the argument value is null or empty
+            if (string.IsNullOrEmpty(value))
+            {
+                return Task.CompletedTask;
+            }
+
+            var stringReader = new StringReader(value);
+            var reader = new CsvReader(stringReader);
+
+            var modelElementType = bindingContext.ModelMetadata.ElementType;
+            var model = reader.GetRecords(modelElementType).ToList();
+
+            bindingContext.Result = ModelBindingResult.Success(model);
+
+            return Task.CompletedTask;
+        }
+    }
+}
