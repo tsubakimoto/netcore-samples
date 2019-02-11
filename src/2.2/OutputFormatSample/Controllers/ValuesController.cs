0f2237a8bf031aac5e4b69af90588e3a0f6ebcde
Yuta Matsumura <tkmbmbr@gmail.com>
2019-02-11 11:16:34 +0900

    応答データの書式設定のサンプル

---
+++ src/2.2/OutputFormatSample/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using GenFu;
+using Microsoft.AspNetCore.Mvc;
+using OutputFormatSample.Models;
+using System.Collections.Generic;
+
+namespace OutputFormatSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> Get()
+        {
+            var persons = A.ListOf<Person>(25);
+            return persons;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
