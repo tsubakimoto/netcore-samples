22ded9e0a8a6eac7e429f4223c39b83f9ee668c0
Yuta Matsumura <tkmbmbr@gmail.com>
2019-02-18 12:53:41 +0900

    ログのサンプルを修正した

--- src/2.2/LoggingSample/Program.cs
+++ src/2.2/LoggingSample/Program.cs
@@ -21,22 +21,30 @@ namespace LoggingSample
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .ConfigureLogging((hostingContext, logging) =>
                 {
                     // Built-in logger
-                    //logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    //logging.AddConsole();
-                    //logging.AddDebug();
+                    logging.ClearProviders();
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    if (hostingContext.HostingEnvironment.IsDevelopment())
+                    {
+                        logging.AddDebug();
+                    }
 
+#if false
                     // Custom logger
-                    //logging.ClearProviders();
-                    //var config = new ColoredConsoleLoggerConfiguration
-                    //{
-                    //    LogLevel = LogLevel.Information,
-                    //    Color = ConsoleColor.Yellow
-                    //};
-                    //logging.AddProvider(new ColoredConsoleLoggerProvider(config));
+                    logging.ClearProviders();
+                    var config = new ColoredConsoleLoggerConfiguration
+                    {
+                        LogLevel = LogLevel.Information,
+                        Color = ConsoleColor.Yellow
+                    };
+                    logging.AddProvider(new ColoredConsoleLoggerProvider(config));
+#endif
 
+#if false
                     // Custom table logger
                     logging.ClearProviders();
                     var config = new TableLoggerConfiguration
@@ -46,6 +54,7 @@ namespace LoggingSample
                         TableName = "mylog"
                     };
                     logging.AddProvider(new TableLoggerProvider(config));
+#endif
                 })
                 .UseStartup<Startup>();
     }
