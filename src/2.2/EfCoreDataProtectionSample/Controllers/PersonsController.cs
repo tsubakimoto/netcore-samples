00298ad49dc4006f979aacaa35992f3656502ac0
Yuta Matsumura <tkmbmbr@gmail.com>
2019-02-18 16:37:51 +0900

    確認用モデルを追加した

---
+++ src/2.2/EfCoreDataProtectionSample/Controllers/PersonsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EfCoreDataProtectionSample.Models;
+
+namespace EfCoreDataProtectionSample.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index([Bind("Id,Name,Birthday")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+    }
+}
