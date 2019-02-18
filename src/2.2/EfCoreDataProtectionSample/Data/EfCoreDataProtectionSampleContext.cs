166865660c66a281adeb208503777f6221184c5b
Yuta Matsumura <tkmbmbr@gmail.com>
2019-02-18 16:51:45 +0900

    データ保護のマイグレーションファイルを作成した

--- src/2.2/EfCoreDataProtectionSample/Data/EfCoreDataProtectionSampleContext.cs
+++ src/2.2/EfCoreDataProtectionSample/Data/EfCoreDataProtectionSampleContext.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EfCoreDataProtectionSample.Models
 {
-    public class EfCoreDataProtectionSampleContext : DbContext
+    public class EfCoreDataProtectionSampleContext : DbContext, IDataProtectionKeyContext
     {
         public EfCoreDataProtectionSampleContext (DbContextOptions<EfCoreDataProtectionSampleContext> options)
             : base(options)
@@ -14,5 +15,7 @@ namespace EfCoreDataProtectionSample.Models
         }
 
         public DbSet<EfCoreDataProtectionSample.Models.Person> Person { get; set; }
+
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
     }
 }
