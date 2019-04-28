47bac86bdad70c242eea0828f78dd82cd959dbba
tsubakimoto <tkmbmbr@gmail.com>
2019-04-28 23:21:42 +0900

    create model and controller

---
+++ src/2.2/SwaggerSample/Data/EmployeeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SwaggerSample.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext (DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SwaggerSample.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ src/2.2/SwaggerSample/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwaggerSample.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+    }
+}
