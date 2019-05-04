5b4b6fe18c7a4dfbf16ed119509ae253d0385247
tsubakimoto <tkmbmbr@gmail.com>
2019-05-04 13:59:30 +0900

    add user secrets sample

--- src/2.2/UserSecretSample/Pages/Index.cshtml.cs
+++ src/2.2/UserSecretSample/Pages/Index.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace UserSecretSample.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string MyPassword { get; private set; }
+
         public void OnGet()
         {
-
+            MyPassword = configuration["MyPassword"];
         }
     }
 }
