87ae7f8c555582ac1fc09199a920c8f9590d4670
tsubakimoto <tkmbmbr@gmail.com>
2019-05-04 16:39:03 +0900

    use GenFu in unit test

--- test/2.2/Solid/CacheRepositorySample.Test/Controllers/EmployeesControllerTest.cs
+++ test/2.2/Solid/CacheRepositorySample.Test/Controllers/EmployeesControllerTest.cs
@@ -1,9 +1,9 @@
 ﻿using CacheRepositorySample.Controllers;
 using CacheRepositorySample.Models;
 using CacheRepositorySample.Repositories;
+using GenFu;
 using Microsoft.Extensions.Caching.Distributed;
 using Moq;
-using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -17,12 +17,7 @@ namespace CacheRepositorySample.Test.Controllers
             var mockRepository = new Mock<IEmployeeRepository>();
             mockRepository
                 .Setup(_ => _.ReadAll())
-                .Returns(new List<Employee>
-                {
-                    new Employee { EmployeeID = 1, Name = "e1" },
-                    new Employee { EmployeeID = 2, Name = "e2" },
-                    new Employee { EmployeeID = 3, Name = "e3" },
-                });
+                .Returns(A.ListOf<Employee>(3));
 
             var mockCache = new Mock<IDistributedCache>();
             var repository = new CacheEmployeeRepository(mockRepository.Object, mockCache.Object);
