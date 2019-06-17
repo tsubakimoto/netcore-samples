385f3eb470bcee56ba4592088371a948d500b5f9
tsubakimoto <tkmbmbr@gmail.com>
2019-06-17 17:24:37 +0900

    Create Azure App Configuration sample

---
+++ src/2.2/AppConfigurationConsoleSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace AppConfigurationConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionStrings:AppConfig"));
+
+            var config = builder.Build();
+            Console.WriteLine(config["AppConfigurationConsoleSample:Settings:Message"] ?? "Hello world!");
+        }
+    }
+}
