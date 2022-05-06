52d929d495efb1a3aa02892b72139043eb12d0ad
tsubakimoto <tkmbmbr@gmail.com>
2022-05-06 13:18:28 +0900

    Create models (Reverse engineering)

---
+++ 6.0/src/EfCoreSample/Models/BuildVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EfCoreSample.Models
+{
+    public partial class BuildVersion
+    {
+        public byte SystemInformationId { get; set; }
+        public string DatabaseVersion { get; set; } = null!;
+        public DateTime VersionDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
