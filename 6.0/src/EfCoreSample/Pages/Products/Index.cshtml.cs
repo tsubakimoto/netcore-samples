fd2178d7d3ca507cdb76046f0939c206bc402731
tsubakimoto <tkmbmbr@gmail.com>
2022-05-07 12:52:24 +0900

    Add pagination of products

--- 6.0/src/EfCoreSample/Pages/Products/Index.cshtml.cs
+++ 6.0/src/EfCoreSample/Pages/Products/Index.cshtml.cs
@@ -14,20 +14,34 @@ namespace EfCoreSample.Pages.Products
     public class IndexModel : PageModel
     {
         private readonly EfCoreSample.Data.SampleDatabaseContext _context;
+        private readonly IConfiguration _configuration;
 
         public string NameSort { get; set; }
         public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public IndexModel(EfCoreSample.Data.SampleDatabaseContext context)
+        public IndexModel(EfCoreSample.Data.SampleDatabaseContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
-        public IList<Product> Product { get;set; }
+        //public IList<Product> Product { get;set; }
+        public PaginatedList<Product> Product { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            if (searchString is not null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
             CurrentFilter = searchString;
 
@@ -55,7 +69,10 @@ namespace EfCoreSample.Pages.Products
                     break;
             }
 
-            Product = await productsIQ.AsNoTracking().ToListAsync();
+            //Product = await productsIQ.AsNoTracking().ToListAsync();
+
+            var pageSize = _configuration.GetValue("PageSize", 4);
+            Product = await PaginatedList<Product>.CreateAsync(productsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
